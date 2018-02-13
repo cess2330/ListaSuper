1b0bf092c744f18e6252c55c92b201c0518a0024
Cesar Castillo <cess_castillo@hotmail.com>
2018-02-13 00:40:34 -0600

    commitInicial

---
+++ ListaSuper/SuperTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SuperTableViewCell.swift
+//  ListaSuper
+//
+//  Created by Cesar Castillo on 12/02/18.
+//  Copyright © 2018 Cesar Castillo. All rights reserved.
+//
+
+import UIKit
+
+class SuperTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
